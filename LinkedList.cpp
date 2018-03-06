1b1a143f5aefcac255ab9e63bc74a90c2377caef
Pavlos Sakoglou <23369146+PavlosSakoglou@users.noreply.github.com>
2018-03-06 09:56:07 -0500

    Fixed typo

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,7 +2,7 @@
 *	© Superharmonic Technologies
 *	Pavlos Sakoglou
 *
-*  ================================================
+*  =================================================
 *
 *	Standard LinkedList API Implementation
 *
